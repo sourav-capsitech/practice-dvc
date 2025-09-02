8c96d16b73713145e245485007e30a949fc34597
sourav-cpasitech <sourav.burnwal@capsitech.com>
2025-09-02 18:09:21 +0530

    3rd version of code

--- mycode.py
+++ mycode.py
@@ -11,6 +11,9 @@ df=pd.DataFrame(data)
 new_row_loc = {"Name": 'GF1', 'Age': 20, "City": 'City1'}
 df.loc[len(df.index)] = new_row_loc
 
+new_loc_2 = {"Name": 'GF2', 'Age': 22, "City": 'City2'}
+df.loc[len(df.index)] = new_loc_2
+
 #ensures the data exist at the rrot level
 data_dir = "data"
 os.makedirs(data_dir, exist_ok=True)
